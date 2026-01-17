a51cc596fc38ae113225fc531a13b4b23b1469f7
samgee <252795767@qq.com>
2026-01-17 17:13:31 +0800

    %202601171713 CMake测试

---
+++ demo/p5/ORM.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 王柏伟 on 2026/1/17.
+//
+
+#include "../../include/ORM.h"
---
+++ include/ORM.h
@@ -0,0 +1,12 @@
+//
+// Created by 王柏伟 on 2026/1/17.
+//
+
+#ifndef LEARNING_ORM_H
+#define LEARNING_ORM_H
+
+class ORM {
+};
+
+
+#endif //LEARNING_ORM_H
